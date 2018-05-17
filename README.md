ed8981668c6f81e09ce0f17f48cbe1257163f5f2
bull321 <39267462+bull321@users.noreply.github.com>
2018-05-17 21:58:29 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# air
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const styles = {
+  fontFamily: 'sans-serif',
+  textAlign: 'center',
+};
+
+const App = () => (
+  <div style={styles}>
+    <h2>Start editing to see some magic happen {'\u2728'}</h2>
+  </div>
+);
+
+export default App
